a4c94a83baa11ea01a1f581d6460b2b8fa0c6bcc
Kevin Gallardo <andreskevin2606@gmail.com>
2023-05-16 16:31:30 -0500

    feat: :rocket: estructura
    agregado lo basico

--- ejercicio crud/nosotros.php
+++ ejercicio crud/nosotros.php
@@ -3,7 +3,25 @@
 
 
 <div>
-    
+<div class="container">
+    <div class="row">
+        <center>
+        <div class="row align-items-md-stretch">
+            <div class="col-md-6">
+                <div
+                    class="h-100 p-5 border rounded-3">
+                    <h2>Nosotros</h2>
+                    <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure
+                        to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both
+                        column's content for equal-height.</p>
+                    <button class="btn Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then,
+                        mix and match with additional component themes and more." type="button">Example button</button>
+                </div>
+            </div>
+        </div>
+        </center>
+    </div>
+</div>
 </div>
 
 
---
+++ ejercicio crud/productos.php
@@ -0,0 +1,16 @@
+<?php include "./template/cabecera.php";  ?>
+
+
+<div class="col-md-3">
+    <div class="card text-white bg-secondary" style="margin-top: 20px;">
+    <img class="card-img-top" src="holder.js/100px180/" alt="Title">
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Text</p>
+    </div>
+    </div>
+</div>
+
+
+
+<?php include "./template/pie.php"; ?>
